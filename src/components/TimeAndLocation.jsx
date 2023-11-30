5d22893759c2f499c05e1f84a9e08b8148872605
Topilogy <36141664+Topilogy@users.noreply.github.com>
2023-11-30 15:43:22 +0100

    draft

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function TimeAndLocation() {
   return (
-    <div>TimeAndLocation</div>
+    <div>
+        <div className='flex items-center justify-center my-6'>
+            <p className='text-white text-xl font-extralight'>
+                Wednesday, 29 Nov 2023 | Local time: 12:08
+            </p>
+        </div>
+
+        <div className='flex items-center justify-center my-3'>
+            <p className='text-white text-3xl font-medium'>
+                Berlin, DE
+            </p>
+        </div>
+    </div>
   )
 }
 
